4ada3c5baf3125bbfefbfdc982ef868132d06cb3
HyeWon33 <hjw141@naver.com>
2020-12-01 01:40:29 +0900

    WF2

---
+++ W_F/script/WF2.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import math
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+
+
+class SelfDrive:
+    def __init__(self, publisher):
+        self.publisher = publisher
+    
+    def lds_callback(self, scan):
+        turtle_vel = Twist()
+        Scan_Value = average(scan.ranges[225:315])
+        Scan_Value_Top = average(scan.ranges[270:315])
+        Scan_Value_Bottom = average(scan.ranges[225:270])
+        Scan_Value_Middle = average(scan.ranges[265:275])
+        print(Scan_Value)
+  
+        if Scan_Value >= 0.15 and Scan_Value <= 0.2:
+            turtle_vel.linear.x = 0.15
+            print("go")
+            
+
+        if (scan.ranges[10:-10] == 0.12):
+            turtle_vel.linear.x = 0
+            turtle_vel.angular.z = (math.pi / 5)
+
+        elif Scan_Value > 0.2:
+            turtle_vel.angular.z = -(math.pi / 4)
+            turtle_vel.linear.x = 0.14 
+            print("Right go")
+            
+
+            if(Scan_Value_Top > Scan_Value_Bottom):
+                turtle_vel.angular.z = -(math.pi / 6)
+                turtle_vel.linear.x = 0.14 
+                print("right go")    
+                
+            else:                
+                turtle_vel.angular.z = -(math.pi / 6) 
+                turtle_vel.linear.x = 0.14
+                print("left right go")
+                
+
+        elif Scan_Value < 1.5:
+            turtle_vel.angular.z = math.pi / 4 
+            turtle_vel.linear.x = 0.12
+            print("Left go")
+           
+
+            if(Scan_Value_Top > Scan_Value_Bottom):
+                turtle_vel.angular.z = -(math.pi / 6)
+                turtle_vel.linear.x = 0.14 
+                print("right go")
+            else:
+                turtle_vel.angular.z = -(math.pi / 6)
+                turtle_vel.linear.x = 0.14 
+                print("right go")
+
+        self.publisher.publish(turtle_vel)
+
+def average(list):
+    return (sum(list) / len(list))
+
+
+def main():
+    rospy.init_node('self_drive')
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+    driver = SelfDrive(publisher)
+    subscriber = rospy.Subscriber('scan', LaserScan,
+                                  lambda scan: driver.lds_callback(scan))
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
+
